fb4282c0a4894b0d316b3f815b6d275a8ba6e535
Matheus Duarte <matheusduartecode@gmail.com>
2025-03-02 21:59:34 -0300

    V2
    • Fav icon
    • Responsiveness
    • New Icons

--- code.js
+++ code.js
@@ -1,32 +1,31 @@
 let sliderState = false;
 
 function toggleSlider() {
-  const dot = document.getElementById('dot');
-  const sun = document.getElementById('sun');
-  const moon = document.getElementById('moon');
-  const body = document.getElementById('body')
-  
+  const dot = document.getElementById("dot");
+  const sun = document.getElementById("sun");
+  const moon = document.getElementById("moon");
+  const body = document.getElementById("body");
+  const footer = document.getElementById("footer");
 
-
-  // Alterar o estado do slider e aplicar a animação
   if (sliderState) {
     sun.style.opacity = "1";
-    dot.style.marginLeft = '15px'; 
-    sun.classList.add('show');
-    moon.classList.remove('show');
+    dot.style.marginLeft = "15px";
+    sun.classList.add("show");
+    moon.classList.remove("show");
     body.style.backgroundColor = "#f1fd50";
     slider.style.backgroundColor = "#f1fd50";
-  
+    footer.style.backgroundColor = "#FFFFFF";
+    footer.style.color = "#000000";
   } else {
-    dot.style.marginLeft = '175px'; 
+    dot.style.marginLeft = `calc(100% - 75px)`;
     sun.style.opacity = "0";
-    sun.classList.remove('show');
-    moon.classList.add('show'); 
+    sun.classList.remove("show");
+    moon.classList.add("show");
     body.style.backgroundColor = "#002176";
     slider.style.backgroundColor = "#002176";
- 
+    footer.style.backgroundColor = "#000000";
+    footer.style.color = "#FFFFFF";
   }
-  
-  // Inverter o estado para a próxima interação
+
   sliderState = !sliderState;
 }
